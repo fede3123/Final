0855f777babc93214ab190cae67287039cd7daf1
Federico Rosero <federik4312@gmail.com>
2022-06-01 00:15:53 -0500

    'Deploy'

--- app.py
+++ app.py
@@ -3,21 +3,29 @@ from werkzeug.utils import secure_filename
 import keras
 import cv2
 import os
-import numpy as np
 
 app = Flask(__name__)
 # Define settings
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-model = keras.models.load_model('Neumonia.h5')
-model.summary()
+neumo = keras.models.load_model('Neumonia.h5')
+neumo.summary()
+sign = keras.models.load_model('Sign.h5')
+sign.summary()
 
 
 def change_diemsions(img):
     img = cv2.imread(f'./static/uploads/{img}', cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (150, 150))
     img_final = img.reshape(-1, 150, 150, 1)
+    return img_final
+
+
+def change_diemsions1(img):
+    img = cv2.imread(f'./static/uploads/{img}', cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (28, 28))
+    img_final = img.reshape(-1, 28, 28, 1)
     return img_final
 
 
@@ -35,9 +43,22 @@ def upload():
     if allowed_file(filename):
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         img = change_diemsions(filename)
-        x = model.predict(img)
+        x = neumo.predict(img)
         print(x)
-        return render_template('predict.html',mensaje='Upload correct', predict=x)
+        return render_template('predict.html', mensaje='Upload correct', predict=x)
+    return "The extensions dont have support"
+
+
+@app.route('/Sign', methods=['POST'])
+def Sign():
+    file = request.files['Sign']
+    filename = secure_filename(file.filename)
+    if allowed_file(filename):
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        img = change_diemsions1(filename)
+        x = sign.predict(img)
+        print(x)
+        return render_template('predict.html', mensaje='Upload correct', predict=x)
     return "The extensions dont have support"
 
 
@@ -47,4 +68,4 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000,debug=False)
